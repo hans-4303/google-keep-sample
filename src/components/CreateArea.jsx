56a6f4fe6576d8df6239070db49b3ee6b1d77e7b
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-10-31 13:26:37 +0900

    객체형 state 생성 및 불변성 유지, 함수 선언 및 호출, 버튼 정의

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateArea = () => {
+  /* 객체 형 state 생성 */
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  /* input, textarea에 연결된 함수 */
+  function handleChange(event) {
+    /* 이벤트 대상에서 name, value 받아오기 */
+    const { name, value } = event.target;
+    /* prevState => return { ...prevState, [name]: value } 형태로 작성됨
+    state의 불변성 유지 */
+    setNote((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+
+  /* button에 연결된 함수 */
+  function submitButton(event) {
+    /* 이벤트 새로 고침 방지 */
+    event.preventDefault();
+    console.log(event);
+  }
+
   return (
     <div>
       <form>
-        <input type="text" placeholder="Title" name="title" />
+        <input
+          value={note.title}
+          type="text"
+          placeholder="Title"
+          name="title"
+          onChange={handleChange}
+        />
         <p>
-          <textarea name="content" placeholder="Take a note..."></textarea>
+          <textarea
+            value={note.content}
+            name="content"
+            placeholder="Take a note..."
+            onChange={handleChange}
+          />
         </p>
+        <button onClick={submitButton}>Add</button>
       </form>
     </div>
   );
