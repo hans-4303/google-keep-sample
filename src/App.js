5400171b191bcf40ce993bf3f50b5d98fcb20a40
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-10-31 12:57:02 +0900

    컴포넌트 생성 및 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import CreateArea from "./components/CreateArea";
 import Header from "./components/Header";
+import Note from "./components/Note";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div>
         <Header />
         <CreateArea />
+        <Note />
       </div>
     </>
   );
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Note = () => {
+  return (
+    <div className="note">
+      <h1>My note</h1>
+      <p>hey what's going on</p>
+    </div>
+  );
+};
+
+export default Note;
