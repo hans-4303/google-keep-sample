f0217bb8015d24b230589a001e25ffd6196e486b
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-10-31 12:06:16 +0900

    Header 컴포넌트 작성 및 호출

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Header = () => {
+  /* create logo for html element, src by google keep */
   const logo = (
     <img
       src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
@@ -14,6 +15,11 @@ const Header = () => {
       <h3>Keep</h3>
     </div>
   );
+  /* === render
+  <div className="header">
+    <img />
+    <h3>Keep</h3>
+  </div> */
 };
 
 export default Header;
